fce5c879c9ba821a2efe982e4b3ee5819e5ecb7e
bobby5develops <bobby5develops@gmail.com>
2017-11-08 16:59:27 -0500

    Envelope Component logic completed

--- src/index.js
+++ src/index.js
@@ -2,15 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 
-//Exercise 1: Create a AddressLabel Component that takes a person object as a prop, and renders
+// Exercise 1: Create a AddressLabel Component that takes a person object as a prop, and renders
 // there name and address.
 
-const personObj = {
-	name: "Robert Yarborough",
-	address: "123 Hoboken, NJ 123456789"
+// Exercise 2: Create a Envelope Component, that takes toPerson, and fromPerson as props.
+// and uses AddressLabel to render address and recipient address with a stamp.
+
+const senderObj = {
+	senderAddress: {
+		name: "Robert Yarborough",
+		address: "123 Hoboken, NJ 123456789"
+	}
+};
+
+const recieverObj = {
+	recieverAddress: {
+		name: "Kharisma Miller",
+		address: "123 Teaneck, NJ 123456789"
+	}
 };
 
 
+
+// Exercise 2 / part 1 - Declare the Envelope Component
+const Envelope = ({toPerson, fromPerson}) => {
+	// declare local variables
+	let {senderAddress} = fromPerson;
+	let {recipientAddress} = toPerson;
+	// render 1 element via the return statement
+	return (
+
+		<div>
+			<AddressLabel person={senderObj.senderAddress}>{senderAddress}</AddressLabel>
+			<AddressLabel person={recieverObj.recieverAddress}>{recipientAddress}</AddressLabel>
+		</div>
+	);
+};
 
 
 
@@ -29,4 +56,4 @@ const AddressLabel =({person}) => {
 
 
 //render Dom elements
-ReactDOM.render(<AddressLabel person={personObj}/>, document.querySelector('#root'));
+ReactDOM.render(<Envelope toPerson={recieverObj} fromPerson={senderObj} />, document.querySelector('#root'));
