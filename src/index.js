b401771f95d979f1b2babdb396d32b601f1356f0
bobby5develops <bobby5develops@gmail.com>
2017-11-08 21:05:31 -0500

    added box-sizing to border-box on all elements in css, also completed coloring and percentage widths

--- src/index.js
+++ src/index.js
@@ -8,18 +8,16 @@ import './styles/index.css';
 // Exercise 2: Create a Envelope Component, that takes toPerson, and fromPerson as props.
 // and uses AddressLabel to render address and recipient address with a stamp.
 
-const senderObj = {
+const letterObj = {
 	senderAddress: {
 		name: "Robert Yarborough",
 		address: "123 Hoboken, NJ 123456789"
-	}
-};
-
-const recieverObj = {
+	},
 	recieverAddress: {
 		name: "Kharisma Miller",
 		address: "123 Teaneck, NJ 123456789"
-	}
+	},
+	stampImg: "Robert Yarborough Studios, LLC."
 };
 
 
@@ -32,9 +30,10 @@ const Envelope = ({toPerson, fromPerson}) => {
 	// render 1 element via the return statement
 	return (
 
-		<div>
-			<AddressLabel person={senderObj.senderAddress}>{senderAddress}</AddressLabel>
-			<AddressLabel person={recieverObj.recieverAddress}>{recipientAddress}</AddressLabel>
+		<div className="Envelope">
+			<AddressLabel person={letterObj.senderAddress}>{senderAddress}</AddressLabel>
+			<AddressLabel person={letterObj.recieverAddress}>{recipientAddress}</AddressLabel>
+			<Stamp hash={letterObj.stampImg} />
 		</div>
 	);
 };
@@ -42,7 +41,7 @@ const Envelope = ({toPerson, fromPerson}) => {
 
 
 
-//declare AddressLabel as a arrow-function component, with destructured prop of object type
+//declare AddressLabel as a arrow-function component, with destructed prop of object type
 const AddressLabel =({person}) => {
 	// declare local variables
 	let {name, address} = person;
@@ -54,6 +53,16 @@ const AddressLabel =({person}) => {
 		</div>);
 };
 
+// declare Stamp as a arrow-function component, with destructed prop of string type
+const Stamp = ({hash})=> {
+	// declare img url
+	let url = `https://s.gravatar.com/avatar/3997db64b950809c5314e8aa83101ade?s=80/${hash}`;
+	// render the img element
+	return (
+		<img src={url} alt="stamp" className="stamp"/>
+	);
+};
+
 
 //render Dom elements
-ReactDOM.render(<Envelope toPerson={recieverObj} fromPerson={senderObj} />, document.querySelector('#root'));
+ReactDOM.render(<Envelope toPerson={letterObj} fromPerson={letterObj} />, document.querySelector('#root'));
